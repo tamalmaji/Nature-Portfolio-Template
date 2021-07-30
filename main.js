94b9d8955e65181219667324f08012a6d40c0e9b
tamalmaji <84242670+tamalmaji@users.noreply.github.com>
2021-07-30 19:11:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Script to open and close sidebar
+function navopen(){
+    document.getElementById("mySidebar").style.display = "block"
+    document.getElementById("myOverlay").style.display = "block"
+}
+
+function navclose(){
+    document.getElementById("mySidebar").style.display = "none"
+    document.getElementById("myOverlay").style.display = "none"
+}
